2d3b2780f71a4e0e7a13f31871f62727bfd57591
Connie Park <conniedc1206@gmail.com>
2022-05-16 21:03:43 -0700

    commit tree 40177380ef40fb78b41e195b76fd2d9812f63be3

--- index.js
+++ index.js
@@ -1,11 +1,36 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+//Use if and else if statements to return the correct greeting based on the distance the passenger desires to travel.
+function scuberGreetingForFeet(ride){
+  if (ride <= 400) {
+    return 'This one is on me!';
+  } 
+  else if (ride > 400 && ride < 2000) {
+    return 'That will be twenty bucks.';
+  } 
+  else if (ride > 2000 && ride < 2500) {
+    return 'I will gladly take your thirty bucks.';
+  } 
+  else if (ride > 2500) {
+    return 'No can do.';
+  }
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
+//Use a ternary operator to return the correct response based on the desired destination of the passenger.
+function ternaryCheckCity(city){
+  return ((city === 'NYC') ? 'Ok, sounds good.' : 'No go.');
 }
 
-function switchOnCharmFromTip(){
-  // Write your code here!
+//Use a switch statement to return a different response based on the generosity of the passenger's tip.
+function switchOnCharmFromTip(tip){
+  
+  switch (tip) {
+    case 'generous':
+      return 'Thank you so much.';
+      break;
+    case 'not as generous':
+      return 'Thank you.';
+      break;
+    case 'thanks for everything':
+      return 'Bye.';
+      break;
+  }
 }
